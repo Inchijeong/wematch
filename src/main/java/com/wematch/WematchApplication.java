9c1f303cf465effcab1dd3f7ebfa772bbd7f5407
Chijeong In <cde0409@naver.com>
2021-04-27 19:37:34 +0900

    프로젝트 생성

---
+++ src/main/java/com/wematch/WematchApplication.java
@@ -0,0 +1,13 @@
+package com.wematch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WematchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WematchApplication.class, args);
+	}
+
+}
